c4b3b1b4432941d200df0a0715c83bc99ae0ff24
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2023-08-10 11:49:32 +0700

    completed app

--- news/urls.py
+++ news/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('author', views.author, name='author'),
-    path('detail/<int:id>', views.detail, name='detail')
+    path('detail/<int:id>', views.detail, name='detail'),
+    path('author/<int:id>', views.author_detail, name='author_detail')
 ]
--- news/views.py
+++ news/views.py
@@ -12,7 +12,14 @@ def home(request):
 
 
 def author(request):
-    return render(request, 'author.html')
+    context = {}
+    authors = models.Author.objects.all().order_by('id')
+    for author in authors:
+        author.prefix_str = getModelChoices(
+            author.prefix, models.name_prefix_choice)
+
+    context['authors'] = authors
+    return render(request, 'author.html', context)
 
 
 def detail(request, id):
@@ -21,3 +28,19 @@ def detail(request, id):
     for article in articles:
         context['article'] = article
     return render(request, 'article_detail.html', context)
+
+
+def author_detail(request, id):
+    context = {}
+    authors = models.Author.objects.filter(id=id)
+    for author in authors:
+        context['author'] = author
+        context['articles'] = models.Article.objects.filter(author=author)
+
+    return render(request, 'author_detail.html', context)
+
+
+def getModelChoices(number, choices):
+    for items in choices:
+        if items[0] == number:
+            return items[1]
