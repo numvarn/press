dd4164f6af26f13a337133e9ccd73ec33bfae65e
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2023-08-24 12:02:49 +0700

    create production phares

--- press/settings_dev.py
+++ press/settings_dev.py
@@ -79,10 +79,10 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
+        'NAME': 'press_db',
+        'USER': 'press',
         'PASSWORD': 'numvarnSc28152456',
-        'HOST': 'db.jgmamafuatavzigaejvk.supabase.co',
+        'HOST': 'localhost',
         'PORT': '',
     }
 }
